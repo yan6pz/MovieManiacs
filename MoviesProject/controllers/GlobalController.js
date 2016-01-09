c90bda5bdeb3a78b5640d5bc1d6ce95b58a70e08
tsvetina-parvanova <tsvetina_parvanova@yahoo.com>
2016-01-09 17:24:55 +0200

    http.post

--- MoviesProject/controllers/GlobalController.js
+++ MoviesProject/controllers/GlobalController.js
@@ -1,3 +1,13 @@
 ﻿moviesApp.controller('GlobalController', function ($scope, $http, $rootScope) {
-  
+    userObject = '';
+
+    surveyInvite = function surveyInvite(userObject) {
+        var objJSON = angular.toJson(userObject, 2);
+        console.log(objJSON);
+        return $http.post('http://localhost:54148/api/users/new', objJSON).then(function (res) {
+            return res;
+        }).catch(function (err) {
+            return err;
+        })
+    }
 });
