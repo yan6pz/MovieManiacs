6db04f32a24101d2137d5161c0876386310e47af
tsvetina-parvanova <tsvetina_parvanova@yahoo.com>
2016-01-09 18:02:11 +0200

    Change function name

--- MoviesProject/controllers/GlobalController.js
+++ MoviesProject/controllers/GlobalController.js
@@ -1,7 +1,7 @@
 ﻿moviesApp.controller('GlobalController', function ($scope, $http, $rootScope) {
     userObject = '';
 
-    surveyInvite = function surveyInvite(userObject) {
+    registerUser = function registerUser(userObject) {
         var objJSON = angular.toJson(userObject, 2);
         console.log(objJSON);
         return $http.post('http://localhost:54148/api/users/new', objJSON).then(function (res) {
