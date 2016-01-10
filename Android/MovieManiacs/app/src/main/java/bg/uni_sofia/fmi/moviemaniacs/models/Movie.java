c395f202531e2d7133025125968866ee308d18b8
Hristo Borisov <hristo@foresightprudence.com>
2016-01-10 18:02:44 +0200

    initial android project

---
+++ Android/MovieManiacs/app/src/main/java/bg/uni_sofia/fmi/moviemaniacs/models/Movie.java
@@ -0,0 +1,13 @@
+package bg.uni_sofia.fmi.moviemaniacs.models;
+
+public class Movie {
+
+    public String Description;
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "Description='" + Description + '\'' +
+                '}';
+    }
+}
