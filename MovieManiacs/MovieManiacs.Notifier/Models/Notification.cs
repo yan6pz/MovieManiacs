1f7f7add048508e0ae2c6519bb7319dd6dde418f
Yanislav Shterev <yan6pz@gmail.com>
2016-01-10 19:13:51 +0200

    Connected notification to front end "rank film"

---
+++ MovieManiacs/MovieManiacs.Notifier/Models/Notification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieManiacs.Notifier.Models
+{
+    public class Notification
+    {
+        public int rank { get; set; }
+        public int userId { get; set; }
+        public string message { get; set; }
+    }
+}
--- MoviesProject/controllers/GlobalController.js
+++ MoviesProject/controllers/GlobalController.js
@@ -2,6 +2,7 @@
     $scope.userObject = {};
     $scope.loginUser = {};
     $scope.currentUser = {};
+    $scope.notifyObject = {};
 
     $scope.registerUser = function registerUser(userObject) {
         var objJSON = angular.toJson(userObject, 2);
@@ -13,6 +14,15 @@
         })
     }
 
+    $scope.notifyUser = function notifyUser(notifyObject) {
+        notifyObject.userId = 2;
+        var objJSON = angular.toJson(notifyObject, 2);
+        return $http.post('http://localhost:52894/notify/send', objJSON).then(function (res) {
+            return res;
+        }).catch(function (err) {
+            return err;
+        })
+    }
     //$scope.login = function login(loginUser) {
     //    var objJSON = angular.toJson(loginUser, 2);
     //    console.log(objJSON);
