b5566291dd1b70ccf25eded1bf0fc8aadd7ed3bd
tsvetina-parvanova <tsvetina_parvanova@yahoo.com>
2016-01-10 23:38:57 +0200

    my movies

--- MoviesProject/controllers/myMoviesController.js
+++ MoviesProject/controllers/myMoviesController.js
@@ -4,9 +4,15 @@ moviesApp.controller('MyMoviesController', function ($scope, $rootScope, $http) 
     $rootScope.showSideMenu = true;
     $http.get('http://localhost:54148/api/movies/all')
        .then(function(res){
-           $scope.myMovies = res.data;
+           $scope.allMovies = res.data;
            console.log(res);
        });
+
+    $http.get('http://localhost:54148/api/usermovies/2')
+   .then(function (res) {
+       $scope.myMovies = res.data;
+       console.log(res);
+   });
        
     $rootScope.selectMovie = function(movie) {
       $rootScope.selectedMovie = movie;
